41da2a6399c9746af00bb74577061d4e0951380d
Tarma <hujing316@aliyun.com>
2023-04-23 22:27:47 +0800

    后台登录

---
+++ hj-admin/src/main/java/com/hj/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.hj.controller;
+
+import com.hj.domain.ResponseResult;
+import com.hj.domain.entity.User;
+import com.hj.enums.AppHttpCodeEnum;
+import com.hj.exception.SystemException;
+import com.hj.serivce.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/user/login")
+    public ResponseResult login(@RequestBody User user){
+        if(!StringUtils.hasText(user.getUserName())){
+            //提示 必须要传用户名
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
+        return loginService.login(user);
+    }
+
+
+
+}
---
+++ hj-framework/src/main/java/com/hj/serivce/LoginService.java
@@ -0,0 +1,9 @@
+package com.hj.serivce;
+
+import com.hj.domain.ResponseResult;
+import com.hj.domain.entity.User;
+
+public interface LoginService {
+    ResponseResult login(User user);
+
+}
